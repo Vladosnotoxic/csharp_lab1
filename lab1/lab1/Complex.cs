2e4616685c401e4fccd3bb8a36805d6c324585fd
Vladosnotoxic <94399193+Vladosnotoxic@users.noreply.github.com>
2022-04-11 22:29:39 +0300

    Add files via upload

---
+++ lab1/lab1/Complex.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    internal class Complex
+    {
+        public double Real { get; set; }
+        public double Imag { get; set; }
+        public void Add(Complex x)
+        {
+            Real += x.Real;
+            Imag += x.Imag;
+        }
+        public void Substract(Complex x)
+        {
+            Real -= x.Real;
+            Imag -= x.Imag;
+        }
+        public void Multiply(Complex x)
+        {
+            Real *= x.Real;
+            Imag *= x.Imag;
+        }
+        public void Divide(Complex x)
+        {
+            Real /= x.Real;
+            Imag /= x.Imag;
+        }
+    }
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string hobby { get; set; }
+        public int Age { get; set; }
+        public int GroupNumber { get; set; }
+
+        public void Add(Student s)
+        {
+            Id += s.Id;
+            Name += s.Name;
+            hobby += s.hobby;
+            Age += s.Age;
+            GroupNumber += s.GroupNumber;
+        }
+        public void AddMore(Student s1, Student s2)
+        {
+            Id += s1.Id;
+            Name += s1.Name;
+            hobby += s1.hobby;
+            Age += s1.Age;
+            GroupNumber += s1.GroupNumber;
+            Id += s2.Id;
+            Name += s2.Name;
+            hobby += s2.hobby;
+            Age += s2.Age;
+            GroupNumber += s2.GroupNumber;
+
+        }
+    }
+}
+
+
---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная работа №1");
+            Console.WriteLine("Студент группы ВИС-31 Солтанов Владислав");
+            Complex a = new Complex() { Real = 3, Imag = 0.5 };
+            Complex b = new Complex() { Real = 2, Imag = 0.8 };
+            a.Add(b);
+            Console.WriteLine($"a = {a.Real} + {a.Imag}i");
+            a.Substract(b);
+            Console.WriteLine($"a = {a.Real} + {a.Imag}i");
+            b.Multiply(a);
+            Console.WriteLine($"b = {b.Real} + {b.Imag}i");
+            b.Divide(a);
+            Console.WriteLine($"b = {b.Real} + {b.Imag}i");
+
+            Student student1 = new Student() { Age = 18, Id = 1, GroupNumber = 31, Name = "Александр", hobby = "Футбольчик" };
+            Student student2 = new Student() { Age = 19, Id = 2, GroupNumber = 32, Name = "Алексей", hobby = "Xоккейчик" };
+
+            student1.Add(student2);
+            student2.AddMore(student1, student2);
+            Console.WriteLine("Student1:");
+            Console.WriteLine($"Id = {student1.Id}, Age = {student1.Age}, Name = {student1.Name}, Hobby = {student1.hobby}, GroupNumber = {student1.GroupNumber}");
+            Console.WriteLine("Student2:");
+            Console.WriteLine($"Id = {student2.Id}, Age = {student2.Age}, Name = {student2.Name},  Hobby= {student2.hobby}, GroupNumber = {student2.GroupNumber}");
+        }
+    }
+}
